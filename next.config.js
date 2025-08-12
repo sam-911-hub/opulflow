52e3054a9c090663f2136054c5e45dcfa4b47962
sam-911-hub <samreigns71@gmail.com>
2025-08-12 15:05:56 +0300

    Simplify next.config.js to fix module resolution

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  experimental: {
+    esmExternals: false
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
